e308248496c243a536f86fc2aad7cc5e35681b04
tijanja <adetunjiakinde@gmail.com>
2021-03-07 11:48:53 +0100

    created the User Data access object, and added the @Entity annotation

---
+++ src/main/java/com/test/test/dao/UserDao.java
@@ -0,0 +1,154 @@
+package com.test.test.dao;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="users")
+public class UserDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true,nullable = false)
+    private String email;
+    @Column(unique = true,nullable = false,length = 11,updatable = false)
+    private String phone;
+    private String password;
+    private String role;
+    private LocalDate dateRegistered;
+    private Integer verified;
+    private LocalDate dateDeactivated;
+    private Integer status;
+
+    public UserDao(){
+
+    }
+
+    public UserDao(String firstName, String lastName, String email, String phone, String password, String role){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+        this.dateRegistered = LocalDate.now();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDate getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public void setDateRegistered(LocalDate dateRegistered) {
+        this.dateRegistered = dateRegistered;
+    }
+
+    public Integer getVerified() {
+        return verified;
+    }
+
+    public void setVerified(Integer verified) {
+        this.verified = verified;
+    }
+
+    public LocalDate getDateDeactivated() {
+        return dateDeactivated;
+    }
+
+    public void setDateDeactivated(LocalDate dateDeactivated) {
+        this.dateDeactivated = dateDeactivated;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDao{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                ", dateRegistered=" + dateRegistered +
+                ", verified=" + verified +
+                ", dateDeactivated=" + dateDeactivated +
+                ", status=" + status +
+                '}';
+    }
+}
