e031c51dc68c52b3c46ddaee1bc4c8940c96840d
tijanja <adetunjiakinde@gmail.com>
2021-03-07 12:11:19 +0100

    Extended the UserDetailsService to create a User detail service for the authentication and authorization process for the REST Api

---
+++ src/main/java/com/test/test/securiy/AuthUser.java
@@ -0,0 +1,30 @@
+package com.test.test.securiy;
+
+import com.test.test.dao.UserDao;
+import com.test.test.dao.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class AuthUser implements UserDetailsService {
+
+    @Autowired
+    UserServiceImpl userService;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserDao userDAO = userService.findUserByEmail(email);
+        if(userDAO==null) throw new UsernameNotFoundException(email);
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(userDAO.getRole()));
+        return User.withUsername(userDAO.getEmail()).password(userDAO.getPassword()).authorities(grantedAuthorities).build();
+    }
+}
