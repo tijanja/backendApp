3489057ea88be7e03d7a3425f5ec6e4881749e43
tijanja <adetunjiakinde@gmail.com>
2021-03-07 12:01:06 +0100

    created the User repository user service file

---
+++ src/main/java/com/test/test/dao/UserService.java
@@ -0,0 +1,23 @@
+package com.test.test.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/** User Service an interface to simplify data persist process */
+
+public interface UserService {
+    // Method to save instance of a user
+    UserDao save(UserDao userDao);
+    // Method to retrieve all users
+    Page<UserDao> getUserList(Pageable pageable);
+    // Method to update the user data
+    UserDao update(UserDao userDao);
+    // Method to retrieve user data by Id
+    UserDao getUserById(UserDao userDao);
+    // Method to deactivate a user by Id
+    void deleteUser(Long id);
+    //get user by their email address
+    UserDao findUserByEmail(String clientId);
+}
