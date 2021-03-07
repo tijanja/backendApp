2cad1ee3f9f17a58acbf3658eda621793f2e5571
tijanja <adetunjiakinde@gmail.com>
2021-03-07 12:48:53 +0100

    Extends the WebSecurityConfigurerAdapter to create a security configuration for the REST API

--- src/main/java/com/test/test/securiy/SecurityConfig.java
+++ src/main/java/com/test/test/securiy/SecurityConfig.java
@@ -1,4 +1,55 @@
 package com.test.test.securiy;
 
-public class SecurityConfig {
+import com.test.test.filter.RequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MyAuthenticationEntryPoint myAuthenticationEntryPoint;
+
+    @Autowired
+    RequestFilter requestFilter;
+
+    @Autowired
+    AuthUser authUser;
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean()
+            throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/oauth/**").permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(myAuthenticationEntryPoint).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Add a filter to validate the tokens with every request
+        http.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(authUser);
+    }
 }
