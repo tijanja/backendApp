e8e9976e2a7ebaedb1738a0b65f1805cb901e4d4
tijanja <adetunjiakinde@gmail.com>
2021-03-07 12:52:45 +0100

    Create a seed data loader to pre load users into the database users table

---
+++ src/main/java/com/test/test/data/SeedDataLoader.java
@@ -0,0 +1,27 @@
+package com.test.test.data;
+
+import com.test.test.dao.UserDao;
+import com.test.test.dao.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SeedDataLoader {
+
+    private static final Logger log = LoggerFactory.getLogger(SeedDataLoader.class);
+
+    @Bean
+    CommandLineRunner initDatabase(UserServiceImpl repository) {
+
+        return args -> {
+            log.info("Preloading " + repository.save(new UserDao("Frank","Eddy","eddy@gmail.com","08099934567","Project123","User")));
+            log.info("Preloading " + repository.save(new UserDao("Adetunji","Akinde","adetunjiakinde@gmail.com","08060774043","Project123","Admin")));
+            log.info("Preloading " + repository.save(new UserDao("Adewale","Adeniji","adewale@gmail.com","0808990987","adewale007","User")));
+            log.info("Preloading " + repository.save(new UserDao("Jude","Elvis","judeelvis@gmail.com","08066634567","elvis_1","User")));
+            log.info("Preloading " + repository.save(new UserDao("Biden","Joe","joebiden@gmail.com","0808990680","biden_usa","Admin")));
+        };
+    }
+}
