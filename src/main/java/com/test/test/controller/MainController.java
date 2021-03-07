5a2934de501e0c1871ed7f473190537ebc51ebfa
tijanja <adetunjiakinde@gmail.com>
2021-03-07 21:31:00 +0100

    edit some of the imported classes

--- src/main/java/com/test/test/controller/MainController.java
+++ src/main/java/com/test/test/controller/MainController.java
@@ -16,7 +16,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 import java.time.LocalDate;
 import java.util.List;
@@ -37,6 +44,9 @@ public class MainController {
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private EmailServiceImpl emailService;
 
     @PostMapping("/oauth/token")
     public ResponseEntity<?> getOauth2Token(@RequestBody Map<String, String> payload){
@@ -113,9 +123,6 @@ public class MainController {
                     return updatedUser;
                 }).get();
     }
-
-    @Autowired
-    EmailServiceImpl emailService;
 
     public void sendmail(){
         emailService.sendSimpleMessage("adetunji.akinde@techadvance.ng","test", "testing....");
