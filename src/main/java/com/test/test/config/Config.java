b361d844c45e23521d059258b9aa3cba44fb529d
tijanja <adetunjiakinde@gmail.com>
2021-03-08 08:00:01 +0100

    removed unused imports

--- src/main/java/com/test/test/config/Config.java
+++ src/main/java/com/test/test/config/Config.java
@@ -10,10 +10,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
 import javax.servlet.ServletContext;
 import java.util.Collections;
 import java.util.Properties;
